dbe16c6130f8255705944b240b8e0b23311392e4
danphi <phittnh@gmail.com>
2023-04-21 15:35:28 +0700

    Initial Commit

---
+++ CardVisitSwiftUI/CardVisitSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardVisitSwiftUIApp.swift
+//  CardVisitSwiftUI
+//
+//  Created by dan phi on 21/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct CardVisitSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
